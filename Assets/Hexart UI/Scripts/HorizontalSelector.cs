0f23a0332a6e284a2468cfff3e37de75b6ecdc9b
Carri Ko <rcwongtk@gmail.com>
2021-02-02 19:23:56 -0700

    First commit

---
+++ Assets/Hexart UI/Scripts/HorizontalSelector.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+namespace Michsky.UI.Hexart
+{
+    public class HorizontalSelector : MonoBehaviour
+    {
+        [Header("RESOURCES")]
+        public Text label;
+        private int index = 0;
+        public int defaultIndex = 0;
+
+        [Header("ELEMENTS")]
+        public List<string> elements = new List<string>();
+
+        [Header("EVENT")]
+        public UnityEvent onValueChanged;
+
+        void Start()
+        {
+            label = transform.Find("Text").GetComponent<Text>();
+            label.text = elements[defaultIndex];
+        }
+
+        public void PreviousClick()
+        {
+            if (index == 0)
+            {
+                index = elements.Count - 1;
+            }
+
+            else
+            {
+                index--;
+            }
+
+            onValueChanged.Invoke();
+            label.text = elements[index];
+        }
+
+        public void ForwardClick()
+        {
+            if ((index + 1) >= elements.Count)
+            {
+                index = 0;
+            }
+
+            else
+            {
+                index++;
+            }
+
+            onValueChanged.Invoke();
+            label.text = elements[index];
+        }
+    }
+}
---
+++ Assets/Hexart UI/Scripts/SplashScreenManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Michsky.UI.Hexart
+{
+    public class SplashScreenManager : MonoBehaviour
+    {
+        [Header("RESOURCES")]
+        public GameObject splashScreen;
+        public GameObject mainPanels;
+        public GameObject homePanel;
+        private Animator mainPanelsAnimator;
+        private Animator homePanelAnimator;
+
+        [Header("SETTINGS")]
+        public bool disableSplashScreen;
+
+        void Start()
+        {
+            if (disableSplashScreen == true)
+            {
+                splashScreen.SetActive(false);
+                mainPanels.SetActive(true);
+
+                mainPanelsAnimator = mainPanels.GetComponent<Animator>();
+                mainPanelsAnimator.Play("Main Panel Opening");
+                homePanelAnimator = homePanel.GetComponent<Animator>();
+                homePanelAnimator.Play("MP Fade-in Start");
+            }
+
+            else
+            {
+                splashScreen.SetActive(true);
+                mainPanels.SetActive(false);
+            }
+        }
+    }
+}
